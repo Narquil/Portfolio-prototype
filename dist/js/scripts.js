e72256d141474e9e163f0aca6b0b6bb849e0876d
Narquil <narquil.crypto@gmail.com>
2022-07-02 14:20:38 +0200

    animation button, animation keyframes & glightbox

---
+++ dist/js/scripts.js
@@ -0,0 +1,6 @@
+const lightbox = GLightbox({
+    "href": "https://www.youtube.com/watch?v=BY9T0pXU1NM",
+    "type": "video",
+    "source": "youtube", //vimeo, youtube or local
+    "width": 900
+});
