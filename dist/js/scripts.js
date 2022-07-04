33325f6612f0ef6251f4e611bbebe5a1648f8e1b
Narquil <narquil.crypto@gmail.com>
2022-07-04 12:14:59 +0200

    companies basic layout & images

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,7 @@
+// glightbox init
 const lightbox = GLightbox({
     "href": "https://www.youtube.com/watch?v=BY9T0pXU1NM",
     "type": "video",
-    "source": "youtube", //vimeo, youtube or local
+    "source": "youtube",
     "width": 900
 });
